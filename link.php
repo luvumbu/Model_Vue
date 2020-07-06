91c4a7b99b06085546e2c1f20e413fd2184bdd88
Luvumbu <luvumbu.n@gmail.com>
2020-07-06 09:57:55 +0200

    Model projets

---
+++ link.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="fontawesome/css/all.css">
+<link rel="stylesheet" href="vue/header/header.css">
+<link rel="stylesheet" href="vue/section/section.css">
+<link rel="stylesheet" href="vue/footer/footer.css">
+
+<script src="vue/header/header.js"></script>
+<script src="vue/section/section.js"></script>
+<script src="vue/footer/footer.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
