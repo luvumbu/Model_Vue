517afbbf1e7d44b2775ea8a1e196374a60223ff6
Luvumbu <luvumbu.n@gmail.com>
2020-07-06 11:03:54 +0200

    AJOUT CLI BOOSTRAPE

--- link.php
+++ link.php
@@ -7,10 +7,12 @@
 <script src="vue/section/section.js"></script>
 <script src="vue/footer/footer.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <script src="vue.js"></script>
 <link rel="stylesheet" href="body_font.css">
 <link rel="stylesheet" href="font.css">
 <link rel="shortcut icon" type="image/png" href="src/img/favicon.png">
+
 <!-- https://fontawesome.com/ -->
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <!--
