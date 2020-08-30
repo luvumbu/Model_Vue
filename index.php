6c9145c459ecb5b9a1304f9ae5ce91ec2cf794aa
Luvumbu <luvumbu.n@gmail.com>
2020-08-30 08:16:32 +0200

    ajout du favicon

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head id="head">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>{{title}}</title>
 </head>
+<link rel="icon" href="https://pbs.twimg.com/profile_images/1244325575659061249/YjvhVutG_400x400.jpg" type="image/gif" sizes="16x16">
 <body>
-
-<?php 
+	<?php
 	include("app.php");
 	include("link.php");
-?>
+	?>
 </body>
+
 </html>
