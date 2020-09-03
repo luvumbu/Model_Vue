061ec9e422ce86686c555216980ec03e5d41ccb1
Luvumbu <luvumbu.n@gmail.com>
2020-09-03 06:22:26 +0200

    ajout des information proprement

--- class/php/data.php
+++ class/php/data.php
@@ -1,16 +1,4 @@
 <?php
+session_start();
 header("Access-Control-Allow-Origin: *");
-// Exemple d'execution du code
-$servername = "localhost";
-$username =$_POST["login"] ; // login utilisateur
-$password =$_POST["password"] ;// mot de passe utilisateur
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
 ?>
--- class/php/ip.php
+++ class/php/ip.php
@@ -1,16 +1,21 @@
 <?php
-//  session_start() ;  
-$localisation = "";
+$localisation = ""; // initiallisation de la variable
 $REMOTE_ADDR =   $_SERVER['REMOTE_ADDR'];
 if ($REMOTE_ADDR == "Localhost" || $REMOTE_ADDR = "127.0.0.1") {
-	$localisation = "localhost";
-	include("class/php/bdd_local.php");
+	include("class/php/bdd_local.php"); // ajout des fichier local
+	$localisation = "localhost"; // atribution delement variable
+	$_SESSION["REMOTE_ADDR"] = $localisation;
+	$_SESSION["country"] = "country_local";
+	$_SESSION["city"] = "city_local";
 } else {
-	$localisation = "internet";
-	include("class/php/bdd_prod.php");
+	include("class/php/bdd_prod.php"); // ajout des fichier prod
+	$localisation = "internet"; // atribution delement variable
+	$_SESSION["REMOTE_ADDR"] = $REMOTE_ADDR;
+	$_SESSION["country"] = $query['country'];
+	$_SESSION["city"] = $query['city'];
 	$ip = $_SERVER['REMOTE_ADDR']; // Recuperation de l'IP du visiteur
 	$query = @unserialize(file_get_contents('http://ip-api.com/php/' . $ip)); //connection au serveur de ip-api.com et recuperation des données
 	if ($query && $query['status'] == 'success') {	//code avec les variables
-		//echo "Bonjour visiteur de " . $query['country'] . "," . $query['city'];
+		echo "Bonjour visiteur de " . $query['country'] . "," . $query['city'];
 	}
 }
