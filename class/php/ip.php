316a4a22887eceea8ef3e7e5a68efa3d615503b8
Luvumbu <luvumbu.n@gmail.com>
2020-09-03 06:03:17 +0200

    changement de nom du web a la prod

--- class/php/ip.php
+++ class/php/ip.php
@@ -7,7 +7,7 @@ if ($REMOTE_ADDR == "Localhost" || $REMOTE_ADDR = "127.0.0.1") {
 	include("class/php/bdd_local.php");
 } else {
 	$localisation = "internet";
-	include("class/php/bdd_local.php");
+	include("class/php/bdd_prod.php");
 	$ip = $_SERVER['REMOTE_ADDR']; // Recuperation de l'IP du visiteur
 	$query = @unserialize(file_get_contents('http://ip-api.com/php/' . $ip)); //connection au serveur de ip-api.com et recuperation des données
 	if ($query && $query['status'] == 'success') {	//code avec les variables
