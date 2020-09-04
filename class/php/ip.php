030523cc3c83d7768a85fce4176bb8359d34953c
Luvumbu <luvumbu.n@gmail.com>
2020-09-04 10:35:16 +0200

    fixation bug affiche lors de la connexion en prod ou locl

--- class/php/ip.php
+++ class/php/ip.php
@@ -1,7 +1,7 @@
 <?php
 $localisation = ""; // initiallisation de la variable
 $REMOTE_ADDR =   $_SERVER['REMOTE_ADDR'];
-if ($REMOTE_ADDR == "Localhost" || $REMOTE_ADDR = "127.0.0.1") {
+if ($REMOTE_ADDR == "localhost" || $REMOTE_ADDR == "127.0.0.1" || $REMOTE_ADDR=="::1" ) {
 	include("class/php/bdd_local.php"); // ajout des fichier local
 	$localisation = "localhost"; // atribution delement variable
 	
@@ -9,15 +9,7 @@ if ($REMOTE_ADDR == "Localhost" || $REMOTE_ADDR = "127.0.0.1") {
 	$_SESSION["country"] = "country_local";
 	$_SESSION["city"] = "city_local";
 } else {
-	include("class/php/bdd_prod.php"); // ajout des fichier prod
-	$localisation = "internet"; // atribution delement variable
-	$_SESSION["REMOTE_ADDR"] = $REMOTE_ADDR;
-	$_SESSION["country"] = $query['country'];
-	$_SESSION["city"] = $query['city'];
-	$ip = $_SERVER['REMOTE_ADDR']; // Recuperation de l'IP du visiteur
-	$query = @unserialize(file_get_contents('http://ip-api.com/php/' . $ip)); //connection au serveur de ip-api.com et recuperation des données
-	if ($query && $query['status'] == 'success') {	//code avec les variables
-		echo "Bonjour visiteur de " . $query['country'] . "," . $query['city'];
-	}
+		include("class/php/bdd_prod.php"); // ajout des fichier prod
+
 }
 $_SESSION["localisation"] = $localisation;
--- link_class.php
+++ link_class.php
@@ -1,5 +1,4 @@
 <script src="class/javascript/data.js"></script>
 <?php 
 include("class/php/ip.php");
-include("class/php/cookie.php"); // ajoute de linformation de l'utilisateur dans la bdd
 ?>
