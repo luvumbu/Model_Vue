20354dff864d68dedae498ad8b116ba5416eac30
Luvumbu <luvumbu.n@gmail.com>
2020-09-14 23:42:41 +0200

    supression echo

--- index.php
+++ index.php
@@ -11,8 +11,7 @@ session_start();
 <link rel="icon" href="https://pbs.twimg.com/profile_images/1244325575659061249/YjvhVutG_400x400.jpg" type="image/gif" sizes="16x16">
 <body id="body">
 	<?php
-   include("link/app.php");
-	echo $_SESSION["localisation"] ;
+   include("link/app.php"); 
 	?>
 </body>
 
